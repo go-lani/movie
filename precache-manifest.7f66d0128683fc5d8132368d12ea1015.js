4fe08fa7dc7002aaf9af46c4d8c83b16645cdd44
publizm <publizm@gmail.com>
2020-01-02 18:38:43 +0900

    Updates

---
+++ precache-manifest.7f66d0128683fc5d8132368d12ea1015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dc37597b843f90231c0f78e753dc04",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "4b5e8fdd45cfe3ac9f06",
+    "url": "/movie/static/css/main.ef910e0b.chunk.css"
+  },
+  {
+    "revision": "e723b04a3ce78ed81dec",
+    "url": "/movie/static/js/2.d48ce34d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie/static/js/2.d48ce34d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b5e8fdd45cfe3ac9f06",
+    "url": "/movie/static/js/main.fed351df.chunk.js"
+  },
+  {
+    "revision": "7cc2fe0439ebe4f0b3d4",
+    "url": "/movie/static/js/runtime-main.031ef244.js"
+  }
+]);
